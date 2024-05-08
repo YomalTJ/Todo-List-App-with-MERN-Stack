0724d25d8b65835c86272bad33ce8cd80b7f8edf
YomalTJ <yomalthushara2295266@gmail.com>
2024-05-08 11:27:39 +0530

    MERN Stack

---
+++ Todo List App/Server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const ToDoModel = require('./Models/ToDo')
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+app.get('/get', (req, res) => {
+    ToDoModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+app.put('/update/:id', (req, res) => {
+    const {id} = req.params;
+    ToDoModel.findByIdAndUpdate({_id: id}, {done: true})
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    ToDoModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+app.post('/add', (req, res) => {
+    const task = req.body.task;
+    ToDoModel.create({
+        task: task
+    }).then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+app.listen(3001, () => {
+    console.log("Server is Running");
+});
